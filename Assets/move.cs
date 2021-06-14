6f6baac0a19e61a2d60b8cb378c7f45d2eac7e42
Hitas <Hitasas@gmail.com>
2021-06-14 15:59:29 +0300

    Initial commit

---
+++ Assets/move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    public float force;
+    float x;
+    public Animator animJump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * force * speed);
+            animJump.Play("Jump");
+        }
+
+        x = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(x, 0, 0) * Time.deltaTime * speed;
+
+        if(x !=0 )
+        {
+            animJump.Play("Walk");
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            animJump.Play("attack");
+        }
+    }
+}
